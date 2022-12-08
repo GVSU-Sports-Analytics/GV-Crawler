8289bfc4ea8083527c2ff2330c0f89c3fa3d013a
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2022-12-07 21:38:14 -0500

    think that I got all the box score links

--- mods/schedule.py
+++ mods/schedule.py
@@ -7,15 +7,36 @@ from scrape import soup, clean, GVSU_PREFIX
 from pprint import pprint
 import datetime
 
-START_URL: str = GVSU_PREFIX + "/sports/baseball/schedule/" + str(datetime.date.today().year)
+START_URL: str = GVSU_PREFIX + \
+                 "/sports/baseball/schedule/" + \
+                 str(datetime.date.today().year)
 
 
 def get_schedule_years():
     s = soup(START_URL)
-    return [GVSU_PREFIX + v["value"] for v in s.find("select", attrs={
-        "id": "sidearm-schedule-select-season"
-    }).find_all("option")]
+    opts = s.find(
+        "select",
+        attrs={"id": "sidearm-schedule-select-season"}
+    ).find_all("option")
+    return [GVSU_PREFIX + v["value"] for v in opts]
+
+
+def box_score_links(yr_link_soup) -> list[str]:
+    a_s = [li.find("a", href=True) for li in yr_link_soup.find_all(
+        "li",
+        attrs={"class": "sidearm-schedule-game-links-boxscore"}
+    )]
+    return [GVSU_PREFIX + i["href"] for i in a_s]
+
+
+# main function of this module
+def schedule():
+    yrs = get_schedule_years()
+    for yr in yrs:
+        yr_sewp = soup(yr)
+        bs = box_score_links(yr_sewp)
+        pprint(bs)
 
 
 if __name__ == "__main__":
-    pprint(get_schedule_years())
+    schedule()
