1eea05f8f7a1b0df6585615cda0bf52e7797fea3
Jensen Holm <96762808+Jensen-holm@users.noreply.github.com>
2023-01-02 14:47:44 -0600

    made the year links method a property

--- roster.py
+++ roster.py
@@ -1,37 +1,42 @@
 from soup.soup import soup, clean_txt
 import datetime
 
-YEAR = str(datetime.datetime.today().year)
-
-BSBL_PREFIX = "https://gvsulakers.com"
-
-START_URL = BSBL_PREFIX + "/sports/baseball/roster/" + YEAR
-
-START_SOUP = soup(START_URL)
-
 
 class Roster:
+    YEAR = str(datetime.datetime.today().year)
+    BSBL_PREFIX = "https://gvsulakers.com"
+    START_URL = BSBL_PREFIX + "/sports/baseball/roster/" + YEAR
+    START_SOUP = soup(START_URL)
 
-    @staticmethod
-    def year_links() -> list[str]:
+    @property
+    def year_links(self) -> list[str]:
         """
         year_links parses through the start_soup constant
         and returns a list of roster links, one for each year
         :return: list of roster links, one for eac season
         """
-
-        roster_select_options = START_SOUP.find("div", attrs={
+        roster_select_options = self.START_SOUP.find("div", attrs={
             "class": "sidearm-roster-select"
         }).find_all("option")
 
         yr_links = []
         for opt in roster_select_options:
-            link = BSBL_PREFIX + opt["value"]
+            link = self.BSBL_PREFIX + opt["value"]
             link = clean_txt(link, " ", "\n", "\t", "\r")
             yr_links.append(link)
         return yr_links
 
     def connect2db(self):
+        return
+
+    @staticmethod
+    def year_loop(year_links: list[str]):
+        for yr in year_links:
+            print(yr)
+        return
+
+    @staticmethod
+    def player_loop():
         return
 
     def update(self):
@@ -40,8 +45,7 @@ class Roster:
         the roster data in the database. This function
         will be run each day.
         """
-        yr_links = self.year_links()
-        print(yr_links)
+        print(self.year_links)
 
 
 if __name__ == "__main__":
